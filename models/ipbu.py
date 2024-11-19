9f0faed001f64ed3645ecb69bc22282f560136cf
Cristian Berrio <cristianberrio9864@gmail.com>
2024-11-19 09:46:13 -0500

    Fix order in fields

--- models/ipbu.py
+++ models/ipbu.py
@@ -100,7 +100,7 @@ class IPBU(models.Model):
 
         # Creamos las líneas de la cotización usando los productos del IPBU
         for product_line in self.product_line_ids:
-            product_description = product_line.product_description or product_line.product_id.name
+            product_description = product_line.description or product_line.name
 
             order_line_vals = {
                 'order_id': sale_order.id,  # Asignamos la cotización creada
